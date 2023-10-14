0460e604eeee5b6081beb037b6553e6789f3d474
PabloMad75 <pablo.madrid.riquelme@gmail.com>
2023-10-14 16:17:11 -0300

    feat update Backend

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import Users from "../models/Users.model.js";
 
 export const getAllUsers = async (req, res) => {
@@ -14,15 +14,17 @@ export const getAllUsers = async (req, res) => {
 export const getUserByEmail = async (req, res) => {
   try {
     const userEmail = req.params.mail; // Obtener el correo electrónico de los parámetros de la solicitud
-    console.log(userEmail)
+    console.log(userEmail);
     const user = await Users.findOne({ emailAddress: userEmail });
     if (!user) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
     console.log(user);
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: 'Error al buscar el usuario por correo electrónico' });
+    res
+      .status(500)
+      .json({ message: "Error al buscar el usuario por correo electrónico" });
   }
 };
 
@@ -47,7 +49,8 @@ export const createUser = async (req, res) => {
       !phoneNumber
     ) {
       console.log(
-        `${(firstName,
+        `${
+          (firstName,
           lastName,
           emailAddress,
           password,
@@ -70,29 +73,29 @@ export const createUser = async (req, res) => {
       password: passwordEncrypt,
       address: address,
       phoneNumber: phoneNumber,
-      role: 'cliente',
+      role: "cliente",
       orders: [],
     });
 
     const setUser = await user.save();
-    res
-      .status(201)
-      .json({
-        user: setUser, // Agrega los datos del usuario al objeto de respuesta
-        message: `Usuario ${setUser.firstName} ${setUser.lastName} fue creado con éxito`,
-      });
-      console.log("datos creados valor setUser ",setUser)
+    res.status(201).json({
+      user: setUser, // Agrega los datos del usuario al objeto de respuesta
+      message: `Usuario ${setUser.firstName} ${setUser.lastName} fue creado con éxito`,
+    });
+    console.log("datos creados valor setUser ", setUser);
   } catch (error) {
     res
       .status(500)
-      .json({ message: `El registro No se logró grabar con éxito ${error.message}` });
+      .json({
+        message: `El registro No se logró grabar con éxito ${error.message}`,
+      });
   }
 };
 
 export const updateUser = async (req, res) => {
   try {
     const {
-      id,
+      _id,
       firstName,
       lastName,
       emailAddress,
@@ -102,63 +105,87 @@ export const updateUser = async (req, res) => {
       role,
     } = req.body;
 
-    if (!firstName || !lastName || !emailAddress || !password || !address || !phoneNumber) {
-      return res.status(400).json({ message: "Debes completar todos los campos" });
+    if (
+      !firstName ||
+      !lastName ||
+      !emailAddress ||
+      !password ||
+      !address ||
+      !phoneNumber
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Debes completar todos los campos" });
     }
 
-    console.log('datos del body',req.body)
-    console.log('valor Id',id)
-    
+    console.log("datos del body", req.body);
+    console.log("valor Id", _id);
+
     const passwordEncrypt = await bcrypt.hash(password, 10);
 
     const updateData = new Users({
+      _id,
       firstName: firstName,
       lastName: lastName,
       emailAddress: emailAddress,
       password: passwordEncrypt,
       address: address,
       phoneNumber: phoneNumber,
-      role: 'cliente',
+      role: "cliente",
       orders: [],
     });
-    
-    console.log('datos del updateData',JSON.stringify(updateData))
-    const updateUser = await Users.findOneAndUpdate({ _id: id }, updateData, { new: true })
-    console.log('datos del updatedUser',JSON.stringify(updateUser))
-    if (!updateUser) {
-      return res.status(404).json({ message: 'Usuario no encontrado' })
-    }
-    res.status(202).json({ updatedUser: updateUser, message: `Usuario ${updateUser.firstName} ${updateUser.lastName} actualizado con éxito` })
 
+    console.log("datos del updateData", JSON.stringify(updateData));
+    const updateUser = await Users.findOneAndUpdate({ _id: _id }, updateData);
+    console.log("datos del updatedUser", JSON.stringify(updateUser));
+    if (!updateUser) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+    res
+      .status(202)
+      .json({
+        updatedUser: updateUser,
+        message: `Usuario ${updateUser.firstName} ${updateUser.lastName} actualizado con éxito`,
+      });
   } catch (error) {
     res.status(500).json({ message: `Falló la actualización` });
   }
 };
 
-
 export const deleteUserByEmail = async (req, res) => {
   try {
     const userMail = req.params.mail;
-    const removeUserByEmail = await Users.findOneAndDelete({ emailAddress: userMail })
+    const removeUserByEmail = await Users.findOneAndDelete({
+      emailAddress: userMail,
+    });
     if (!updateUser) {
-      return res.status(404).json({ message: 'Usuario no encontrado' })
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-    res.status(202).json({ message: `Usuario ${removeUserByEmail.firstName} ${removeUserByEmail.lastName} eliminado con éxito` })
+    res
+      .status(202)
+      .json({
+        message: `Usuario ${removeUserByEmail.firstName} ${removeUserByEmail.lastName} eliminado con éxito`,
+      });
   } catch (error) {
     res
       .status(500)
       .json({ message: `El registro No se logró eliminar  con éxito` });
   }
-}
-
+};
 
 export const login = async (req, res) => {
   try {
     const { emailAddress, password } = req.body;
 
-    const verifyUserByCorreo = await Users.findOne({ emailAddress: emailAddress })
+    const verifyUserByCorreo = await Users.findOne({
+      emailAddress: emailAddress,
+    });
     if (!verifyUserByCorreo) {
-      return res.status(404).json({ message: `El correo de usuario '${emailAddress}' no existe en nuestra base de datos` });
+      return res
+        .status(404)
+        .json({
+          message: `El correo de usuario '${emailAddress}' no existe en nuestra base de datos`,
+        });
     }
 
     console.log(`Usuario del front: ${emailAddress}`);
@@ -167,17 +194,16 @@ export const login = async (req, res) => {
     console.log(`Contraseña del front: ${password}`);
     console.log(`Contraseña del backend: ${verifyUserByCorreo.password}`);
 
-    // if (password !== verifyUserByCorreo.password) {
-    //   return res.status(403).json({ message: 'La contraseña es incorrecta' });
-    // }
-
-    
-    const verifyPassword = await bcrypt.compare(password, verifyUserByCorreo.password)
-    if(!verifyPassword){
-      return res.status(401).json({message:'Contraseña incorrecta'})
+    const verifyPassword = await bcrypt.compare(
+      password,
+      verifyUserByCorreo.password
+    );
+    if (!verifyPassword) {
+      return res.status(401).json({ message: "Contraseña incorrecta" });
     }
 
-    const {_id, firstName, lastName, address,phoneNumber,role } = verifyUserByCorreo
+    const { _id, firstName, lastName, address, phoneNumber, role } =
+      verifyUserByCorreo;
 
     const token = {
       _id,
@@ -187,16 +213,18 @@ export const login = async (req, res) => {
       password,
       address,
       phoneNumber,
-      role:role
-    }
-console.log('valor del token',JSON.stringify(token))
-    res.json(token)
+      role: role,
+    };
+    console.log("valor del token", JSON.stringify(token));
+    res.json(token);
     //  else {
     //   return res.status(200).json({ message: `La contraseña es correcta, bienvenido ${verifyUserByCorreo.firstName}` });
     // }
-
-    // Resto de tu código para generar el token...
   } catch (error) {
-    res.status(403).json({ message: `No pudimos verificar tu cuenta debido a un error: ${error.message}` });
+    res
+      .status(403)
+      .json({
+        message: `No pudimos verificar tu cuenta debido a un error: ${error.message}`,
+      });
   }
-}
+};
